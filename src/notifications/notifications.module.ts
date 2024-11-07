980dfb598af0dc466ad84f5c3cdf7f9298969b42
amal <amalraju9598@gmail.com>
2024-11-07 13:02:31 +0530

    chore(notifications): engagespot

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
+import { EngagespotService } from './services/engagespot.service';
 
 @Module({
   controllers: [NotificationsController],
-  providers: [NotificationsService],
+  providers: [NotificationsService, EngagespotService],
+
 })
-export class NotificationsModule {}
+export class NotificationsModule { }
